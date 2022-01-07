1ec4e16d586c9f1695ae842234c979062b010fe6
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-07 08:36:25 +0500

    Create foods controllers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+//= require bootstrap-sprockets
+//= require toastr
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
