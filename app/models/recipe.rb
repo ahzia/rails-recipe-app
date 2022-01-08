bc328808b464ec299ab336fbb5d66dd41d3c50ac
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-09 02:32:00 +0500

    Validate Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
+  validates :name, presence: { message: 'Please enter a name' }
+  validates :preparation_time, presence: { message: 'Please enter a preparation time' }
+  validates :cooking_time, presence: { message: 'Please enter a cooking time' }
 end
