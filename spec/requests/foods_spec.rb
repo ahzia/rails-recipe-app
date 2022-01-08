a0ca29240168e374058628ffc191db62d7cab4f9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-09 03:29:10 +0500

    Do not show unless log in

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -2,16 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
   describe 'GET /foods' do
-    it 'works! (now write some real specs)' do
+    it 'does not allow unauthenticated users to see foods' do
       get foods_path
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  describe 'GET /foods/new' do
-    it 'works! (now write some real specs)' do
-      get new_food_path
-      expect(response).to have_http_status(200)
+      expect(response).to redirect_to new_user_session_path
     end
   end
 end
