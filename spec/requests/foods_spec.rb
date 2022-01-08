1cb12213429aa2c050e472fa9006552b681abdf9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-08 21:53:44 +0500

    Add tests for integrations

---
+++ spec/requests/foods_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Foods", type: :request do
+  describe "GET /foods" do
+    it "works! (now write some real specs)" do
+      get foods_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /foods/new" do
+    it "works! (now write some real specs)" do
+      get new_food_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
