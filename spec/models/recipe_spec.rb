dc6373b3d04e8f85b8b207bbe068ca989d085804
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-09 03:05:51 +0500

    Delete unused commits

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,9 +1,4 @@
 require 'rails_helper'
-# class Recipe < ApplicationRecord
-#   belongs_to :user
-#   has_many :recipe_foods, dependent: :destroy
-#   has_many :foods, through: :recipe_foods
-# end
 
 RSpec.describe Recipe, type: :model do
   it 'belongs to a user' do
