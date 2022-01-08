95515e4c635e604c7b4927c10ab750b660556f7b
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-09 02:48:47 +0500

    Update food_spec.rb

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe Food, type: :model do
   it 'belongs to a user' do
     food = Food.new
@@ -8,5 +9,10 @@ RSpec.describe Food, type: :model do
   it 'has a name' do
     food = Food.create(name: 'Pizza')
     expect(food.name).to eq('Pizza')
+  end
+
+  it 'has a measurement unit' do
+    food = Food.create(measurement_unit: 'g')
+    expect(food.measurement_unit).to eq('g')
   end
 end
