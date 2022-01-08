5765500ec4d562c3c8feb37228eaebacf4974ad9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-08 22:03:08 +0500

    Delete unused commits

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
-# class Food < ApplicationRecord
-#   belongs_to :user
-#   validates :name , presence: { message: 'Please enter a name' }
-#   validates :measurement_unit , presence: { message: 'Please enter a measurement unit' }
-#   validates :price , presence: { message: 'Please enter a price' }
-#   has_many :recipe_foods, dependent: :destroy
-# end
 RSpec.describe Food, type: :model do
   it 'belongs to a user' do
     food = Food.new
