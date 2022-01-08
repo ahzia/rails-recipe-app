bbcf6510b75c1577ee78e4d528202d1e7d7a2c7a
Ahmad Zia Yousufi <ah.ziayosfi@gmail.com>
2022-01-08 21:49:13 +0430

    Revert "Recipt"

--- db/migrate/20220108113607_add_description_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToRecipes < ActiveRecord::Migration[6.1]
-  def change
-    add_column :recipes, :description, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_113607) do
+ActiveRecord::Schema.define(version: 2022_01_07_035540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2022_01_08_113607) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "description"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
