1b96d814a9fa28a834faa8a931b2f9e090cb6dee
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-08 21:56:58 +0500

    Run linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,51 +1,51 @@
 class RecipesController < ApplicationController
-    def index
-        @recipes= Recipe.where(user_id: current_user.id)
+  def index
+    @recipes = Recipe.where(user_id: current_user.id)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      flash[:notice] = 'Recipe added successfully'
+      redirect_to recipes_path
+    else
+      render :new
     end
-    
-    def new
-      @recipe = Recipe.new
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:notice] = 'Recipe updated successfully'
+      redirect_to recipes_path
+    else
+      render :edit
     end
-  
-    def create
-      @recipe = Recipe.new(recipe_params)
-      @recipe.user = current_user
-      if @recipe.save
-        flash[:notice] = 'Recipe added successfully'
-        redirect_to recipes_path
-      else
-        render :new
-      end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user_id == current_user.id
+      @recipe.destroy
+      flash[:notice] = 'Recipe deleted successfully'
+      redirect_to recipes_path
+    else
+      flash[:notice] = 'Only deletable by the owner'
     end
-  
-    def edit
-      @recipe = Recipe.find(params[:id])
-    end
-  
-    def update
-      @recipe = Recipe.find(params[:id])
-      if @recipe.update(recipe_params)
-        flash[:notice] = 'Recipe updated successfully'
-        redirect_to recipes_path
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @recipe = Recipe.find(params[:id])
-      if @recipe.user_id=current_user.id
-        @recipe.destroy
-        flash[:notice] = 'Recipe deleted successfully'
-        redirect_to recipes_path
-      else
-        flash[:notice] = 'Only deletable by the owner'
-      end
-    end
-  
-    private
-  
-    def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public)
-    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public)
+  end
 end
--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Foods", type: :request do
-  describe "GET /foods" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Foods', type: :request do
+  describe 'GET /foods' do
+    it 'works! (now write some real specs)' do
       get foods_path
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /foods/new" do
-    it "works! (now write some real specs)" do
+  describe 'GET /foods/new' do
+    it 'works! (now write some real specs)' do
       get new_food_path
       expect(response).to have_http_status(200)
     end
